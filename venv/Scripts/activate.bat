91f8d1f0ba647636913389b3b2f06f512fef9fc0
jrahappy <william.bh.lee@gmail.com>
2025-03-29 00:03:47 -0400

    test

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Dev\dj_vite\venv
+set VIRTUAL_ENV=D:\Dev\radchat\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
